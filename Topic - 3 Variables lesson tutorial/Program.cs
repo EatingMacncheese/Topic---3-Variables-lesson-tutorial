a62ecaf0b507108f4c6e5c6b9a286834197a34c3
Tiago Teague Violante <339784910@GoTVDSB.ca>
2024-09-12 09:08:16 -0400

    done tutorial

--- Topic - 3 Variables lesson tutorial/Program.cs
+++ Topic - 3 Variables lesson tutorial/Program.cs
@@ -9,6 +9,7 @@
             int myAge, myHeight, myWeight;
 
             myName = "Tiago";
+            myAge = 16;
             myHeight = 71; //inches
             myWeight = 150; //I dont really know my exact weight
             myEyes = "brown";
@@ -16,13 +17,47 @@
             myHair = "black";
 
             Console.WriteLine("Let's talk about " + myName + ".");
-            Console.WriteLine("He's " + (myHeight) + " inches tall.");
-            Console.WriteLine("he weighs " + myWeight + " pounds.");
-            Console.WriteLine("he has" + myEyes + "eyes and " + myHair + " hair.");
-            Console.WriteLine("his teeth are pretty much" + myTeeth);
+            Console.WriteLine("He is " + myAge + " years old");
+            Console.WriteLine("He's " + (myHeight) + + (myHeight * 2.54) + "cm tall.");
+            Console.WriteLine("He weighs " + myWeight + " pounds or " + (myWeight / 2.2) + " kg heavy.");
+            Console.WriteLine("He has " + myEyes + " eyes and " + myHair + " hair.");
+            Console.WriteLine("and his teeth are pretty much " + myTeeth);
 
             Console.ReadLine();
 
+            string name = "johnathan";
+            Console.WriteLine(name);
+            name = "jon";
+            Console.WriteLine(name);
+
+            Console.WriteLine();
+            Console.WriteLine();
+
+            int myNumber = 15;
+            myNumber = 20;
+            Console.WriteLine(myNumber);
+
+            Console.WriteLine();
+            Console.WriteLine();
+
+            double height = 1.62;
+            Console.WriteLine(height);
+
+            Console.WriteLine();
+            Console.WriteLine();
+
+
+            Console.WriteLine("The diference between int and double in a data type is");
+            Console.WriteLine("that in is a 32 bit data type used to store and integer,");
+            Console.WriteLine("and double is a 64 bit data type used to store flowting points");
+
+            Console.WriteLine();
+            Console.WriteLine();
+
+            int age = 15;
+            Console.WriteLine(age / 2);
+
+            Console.ReadLine();
         }
     }
 }
