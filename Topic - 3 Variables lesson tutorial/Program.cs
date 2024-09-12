a10092dc6228d20f4a9f29fb249243ce4b6c49cb
Tiago Teague Violante <339784910@GoTVDSB.ca>
2024-09-12 08:35:26 -0400

    Add project files.

---
+++ Topic - 3 Variables lesson tutorial/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace Topic___3_Variables_lesson_tutorial
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Tiago
+            String myName, myEyes, myTeeth, myHair;
+            int myAge, myHeight, myWeight;
+
+            myName = "Tiago";
+            myHeight = 71; //inches
+            myWeight = 150; //I dont really know my exact weight
+            myEyes = "brown";
+            myTeeth = "white";
+            myHair = "black";
+
+            Console.WriteLine("Let's talk about " + myName + ".");
+            Console.WriteLine("He's " + (myHeight) + " inches tall.");
+            Console.WriteLine("he weighs " + myWeight + " pounds.");
+            Console.WriteLine("he has" + myEyes + "eyes and " + myHair + " hair.");
+            Console.WriteLine("his teeth are pretty much" + myTeeth);
+
+            Console.ReadLine();
+
+        }
+    }
+}
